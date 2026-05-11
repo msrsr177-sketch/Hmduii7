2c7872d2eea45f637ab9f7e5a514bf9f506484c4
msrsr177-sketch <msrsr177@gmail.com>
2026-05-11 04:54:58 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.msrsr.ai',
-  appName: 'نيو فون قسم الصيانه',
+  appName: 'نيو فون  ',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
